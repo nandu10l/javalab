196b0bdac71b82176425d6110127ec5c73507eff
nandu10l <118618712+nandu10l@users.noreply.github.com>
2023-09-26 14:36:00 +0530

    Add files via upload

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,18 @@
+package javalab;
+import java.util.Scanner;
+public class Sumoftwonumbers {
+	public static void main(String[] args) {
+		int num1,num2,sum=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the first number:");
+		
+		num1=sc.nextInt();
+		System.out.println("enter the second number:");
+		
+		num2=sc.nextInt();
+		sum=sum+num1+num2;
+		
+		System.out.println("the sum is :"+sum);
+	}
+
+}

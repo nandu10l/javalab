ee68c9d4914c69f98655419b5ebc1cd2d701c70f
nandu10l <118618712+nandu10l@users.noreply.github.com>
2023-09-26 14:53:07 +0530

    Add files via upload

---
+++ Attendancemark.java
@@ -0,0 +1,28 @@
+
+/*
+ * File          :Attendancemark.java 
+ * Description   : Find attendance mark
+ * Author        :Nandagopan L
+ * Version       : 1.0
+ * Date          : 26/09/2023
+ * */
+package javalab;
+import java.util.Scanner;
+public class Attendancemark {
+	public static void main(String[] args) {
+		float attendancepercentage;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the attendance percentage:");
+		attendancepercentage=sc.nextInt();
+		if(attendancepercentage>90)
+		{
+			System.out.println("the mark is :10");
+		}
+		else
+		{
+			float newmark=attendancepercentage/10;
+			System.out.println("the mark is :"+newmark);
+			
+		}
+	}
+}

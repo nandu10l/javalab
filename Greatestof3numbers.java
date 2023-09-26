ba620193fb50357f614c75bc8aadbb3da7c02c8a
nandu10l <118618712+nandu10l@users.noreply.github.com>
2023-09-26 15:08:43 +0530

    Add files via upload

---
+++ Greatestof3numbers.java
@@ -0,0 +1,39 @@
+
+/*
+ * File          :Attendancemark.java 
+ * Description   : Find attendance mark
+ * Author        :Nandagopan L
+ * Version       : 1.0
+ * Date          : 26/09/2023
+ * */package javalab;
+import java.util.Scanner;
+public class Greatestof3numbers {
+	public static void main (String[] args) {
+		int num1,num2,num3;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the first number:");
+		num1=sc.nextInt();
+		System.out.println("enter the second number:");
+		num2=sc.nextInt();
+		System.out.println("enter the third number:");
+		num3=sc.nextInt();
+		if (num1==num2&&num1==num3)
+		{
+			System.out.println("The  numbers are equal ");
+		}
+		else if(num1>num2&&num1>num3)
+		{
+			System.out.println("the greatest number is:"+num1);
+		}
+		else if(num2>num3)
+		{
+			System.out.println("the greatest number is:"+num2);
+		}
+		else
+		{
+			System.out.println("the greatest number is:"+num3);
+		}
+	}
+	
+
+}
